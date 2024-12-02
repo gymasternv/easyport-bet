8258b437c24bf956101ebc48c9a0e3d3146bcbac
gymasternv <admin@easysportbet.co.uk>
2024-12-02 09:30:52 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('show-email-btn').addEventListener('click', function () {
-    // Construct the email dynamically
-    const emailParts = ['support', 'easysportbet', 'co.uk'];
-    const email = `${emailParts[0]}@${emailParts[1]}.${emailParts[2]}`;
-    
-    // Display the email in the paragraph
-    document.getElementById('email-display').innerText = email;
-});
