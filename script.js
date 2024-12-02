3c0a82dfb228f22aecdcad4186a2a51506cf594d
gymasternv <admin@easysportbet.co.uk>
2024-12-02 09:20:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.getElementById('show-email-btn').addEventListener('click', function () {
-    const email = ['support', 'easysportbet', 'co.uk'].join('@').replace('support@', 'support.');
+    // Construct the email dynamically
+    const emailParts = ['support', 'easysportbet', 'co.uk'];
+    const email = `${emailParts[0]}@${emailParts[1]}.${emailParts[2]}`;
+    
+    // Display the email in the paragraph
     document.getElementById('email-display').innerText = email;
 });
